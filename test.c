89d88ec4845f75e2536f466e9646f0914fd76377
zhc <1553986076@qq.com>
2019-11-08 17:01:57 +0800

    hook read

--- test.c
+++ test.c
@@ -1,9 +1,13 @@
-#include<linux/init.h>
-#include<linux/module.h>
-#include<linux/version.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/unistd.h>
+#include <linux/utsname.h>
+#include <asm/pgtable.h>
 #include <linux/kallsyms.h>
+#include <linux/proc_fs.h>
+#include<linux/init.h>
+#include<linux/version.h>
 #include<linux/moduleparam.h>
-#include<linux/unistd.h>
 #include<linux/sched.h>
 #include<linux/syscalls.h>
 #include<linux/string.h>
@@ -15,124 +19,116 @@
 #include <linux/slab.h>
 #include <linux/mm.h>
 #include <linux/fs_struct.h>
-#include <linux/limits.h>
-
-#define AUDITPATH "/home/test/file_safe"  //保险箱文件
-#define MAX_LENGTH 256
-
+#include <linux/limits.h> 
+/*
+** module macros
+*/
 MODULE_LICENSE("GPL");
-unsigned long** hhh;
-asmlinkage long(* origin_openat)(struct pt_regs *regs);
-void get_fullname(const char *pathname,char *fullname);
-int judge_path(struct pt_regs * regs, char * pathname);
-
-asmlinkage long new_openat(struct pt_regs *regs)//新的openat
-{
-    
-	char buffer[PATH_MAX];
-	long nbytes;
-	nbytes=strncpy_from_user(buffer,(char*)regs->bx,PATH_MAX);
-    
-	if(judge_path(regs,buffer))  //判断是否保险箱safe_file 
+MODULE_AUTHOR("test");
+MODULE_DESCRIPTION("hook sys_read");
+ 
+/*
+** module constructor/destructor
+*/
+//typedef void (*sys_call_ptr_t)(void);
+//sys_call_ptr_t *_sys_call_table = NULL;
+unsigned long sys_call_table_addr = 0;
+typedef asmlinkage long (*orig_read)(struct pt_regs *regs);
+orig_read old_read = NULL;
+ 
+// hooked mkdir function
+asmlinkage long hooked_read(struct pt_regs *regs) {
+    struct files_struct *files=current->files;
+    int i=0;
+	char *buf=(char*)kmalloc(2048,GFP_KERNEL);
+	
+	if(buf==NULL)
+		return old_read(regs);
+	
+	if(NULL!=files)
 	{
-		if(strncmp(current->comm ,"safe_manager",10)!=0)    //判断是否管理程序
+		struct fdtable* fdt=files->fdt;				
+		i=0;
+		for(;i<NR_OPEN_DEFAULT;i++)//默认打开表是NR_OPEN_DEFAULT
 		{
-			printk(KERN_ALERT"this is a open\n");
-			return -1;
-			//nbytes=strncpy_from_user(buffer,(char*)regs->bx,PATH_MAX);
+			char* path=NULL;
+			struct file* file=fdt->fd[i];
+			if(file!=NULL&&file->f_path.dentry!=NULL)
+			{	
+				char*result=NULL;
+				if(file->f_path.dentry->d_iname!=NULL)
+				{
+					//result=strstr(file->f_path.dentry->d_iname,"file_safe");//查找制定类型的文件
+					path=dentry_path_raw(file->f_path.dentry,buf, 2048);
+					if(strstr(path,"file_safe"))//识别出保险箱
+						return -1;
+				}
+			}
+		}
 
-			//printk("%s",*((char __user * __user *)(regs->cx)+1));
-		} 
+
+	}
+	kfree(buf);
+
+    return old_read(regs);
+}
+
+
+
+static int obtain_sys_call_table_addr(unsigned long * sys_call_table_addr) {
+	int ret = 1;
+	unsigned long temp_sys_call_table_addr;
+ 
+	temp_sys_call_table_addr = kallsyms_lookup_name("sys_call_table");
+	
+	/* Return error if the symbol doesn't exist */
+	if (0 == sys_call_table_addr) {
+		ret = -1;
+		goto cleanup;
 	}
 	
-	return origin_openat(regs);
-
+	printk("Found sys_call_table: %p", (void *) temp_sys_call_table_addr);
+	*sys_call_table_addr = temp_sys_call_table_addr;
+		
+cleanup:
+	return ret;
 }
 
-void get_fullname(const char *pathname,char *fullname)
-{
-	struct dentry *parent_dentry = current->fs->pwd.dentry;
-        char buf[MAX_LENGTH];     
-	if (*(parent_dentry->d_name.name)=='/'){
-	    strcpy(fullname,pathname);
-	    return;
-	}	
-	for(;;){
-	    if (strcmp(parent_dentry->d_name.name,"/")==0)
-		buf[0]='\0';
-	    else
-	        strcpy(buf,parent_dentry->d_name.name);
-            strcat(buf,"/");
-            strcat(buf,fullname);
-            strcpy(fullname,buf);
-            if ((parent_dentry == NULL) || (*(parent_dentry->d_name.name)=='/'))
-                 break;
-            parent_dentry = parent_dentry->d_parent;
+unsigned int level;
+pte_t *pte;
+ 
+// initialize
+static int hooked_init(void) {
+    printk("+ Loading hook_mkdir module\n");
+	int ret = -1;
+	ret = obtain_sys_call_table_addr(&sys_call_table_addr);
+    if(ret != 1){
+	printk("- unable to locate sys_call_table\n");
+	return 0;
 	}
-	strcat(fullname,pathname);
-	return;
+ 
+    printk("+ found sys_call_table at %08lx!\n", sys_call_table_addr);
+    old_read = ((unsigned long * ) (sys_call_table_addr))[__NR_read]; 
+    pte = lookup_address((unsigned long) sys_call_table_addr, &level);
+ 
+    set_pte_atomic(pte, pte_mkwrite(*pte));//写保护
+ 
+    printk("+ unprotected kernel memory page containing sys_call_table\n");
+    ((unsigned long * ) (sys_call_table_addr))[__NR_read]= (unsigned long) hooked_read;
+    printk("+ sys_read hooked!\n");
+ 
+    return 0;
 }
-
-int judge_path(struct pt_regs * regs, char * pathname)
-{
-    char fullname[PATH_MAX];
-    char auditpath[PATH_MAX];
-    memset(fullname, 0, PATH_MAX);
-    memset(auditpath, 0, PATH_MAX);
-    get_fullname(pathname,fullname);
-//    printk("Info: fullname is  %s \n",fullname);
-    strcpy(auditpath,AUDITPATH);
-    if (strncmp(fullname,auditpath,12) == 0) 
-	    {
-		printk("right");
-		return 1;
-		}
-	else
-		return 0;
+ 
+static void hooked_exit(void) {
+    if(old_read != NULL) {
+      
+        ((unsigned long * ) (sys_call_table_addr))[__NR_read] = (unsigned long) old_read;
+        set_pte_atomic(pte, pte_clear_flags(*pte, _PAGE_RW));//写保护
+    }
+    
+    printk("+ Unloading hook_read module\n");
 }
-//内存写保护
-void disable_write_protection(void)
-{
-  unsigned long cr0 = read_cr0();
-  clear_bit(16, &cr0);
-  write_cr0(cr0);
-  printk("disable cr0%lx\n",(unsigned long)cr0);
-}
-
-void enable_write_protection(void)
-{
-  unsigned long cr0 = read_cr0();
-  set_bit(16, &cr0);
-  write_cr0(cr0);
-  printk("enable cr0%lx\n",(unsigned long)cr0);
-
-}
-static int __init hello_init(void)
-{
-    hhh=kallsyms_lookup_name("sys_call_table");
-
-   disable_write_protection();
-   origin_openat=(void *)hhh[__NR_openat];
-   printk("before %lx\n",(unsigned long)(hhh[__NR_openat]));
-   hhh[__NR_openat]=(unsigned long*)new_openat;
-   printk("openat %lx\n",(unsigned long)origin_openat);
-   printk("new openat %lx\n",(unsigned long)new_openat);
-   printk("after %lx\n",(unsigned long)(hhh[__NR_openat])); 
-   enable_write_protection();
-   {
-   printk("%d\n",__NR_openat);
-   }
-   printk("dasfd %lx\n",(unsigned long)hhh);
- return 0;
-}
-
-static void __exit hello_exit(void)
-{
-    disable_write_protection();
-    hhh[__NR_openat] = (unsigned long*)origin_openat;
-	enable_write_protection(); 
-	printk(KERN_ALERT "Goodbye \n");
-}
-
-module_init(hello_init);
-module_exit(hello_exit);
+ 
+module_init(hooked_init);
+module_exit(hooked_exit);
