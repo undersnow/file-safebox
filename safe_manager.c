86af514328254fa5a66b0c735f701937a16f116d
lvshengwei <763098304@qq.com>
2019-12-08 20:26:39 +0800

    update safe manager

--- safe_manager.c
+++ safe_manager.c
@@ -1,15 +1,18 @@
-#include<unistd.h>
-#include<fcntl.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<stdio.h>			
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>			
 #include <dirent.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 
 #define SAFEPATH "/root/safeBox"
+#define PASSWORD "password"
 
+char password[80], cmd1[80], cmd2[80];
+char cwd[1024];
 
 void getFileName(char * dirPath)
 {
@@ -42,122 +45,153 @@ void getFileName(char * dirPath)
 	chdir("..");
 }
 
+int copyFile(char *from, char *to) {
+	int in = -1, out = -1, flag;
+	char buffer[1024];
+
+	chdir(cwd);
+
+	printf("copy from %s to %s\n", from, to);
+
+	in = open(from,S_IWUSR);
+	if(in == -1)
+	{
+		printf("open file %s failed!\n", from);
+		return -1;
+	} 
+
+	out = creat(to,S_IWUSR|S_IRUSR);
+	if (out == -1)
+	{
+		printf("create file %s failed!\n", to);
+		return -1;
+	}
+
+	// copy file content
+	while((flag=read(in,buffer,1024))>0)
+	{
+		write(out,buffer,flag);
+	}
+
+	close(in);
+	close(out);
+
+	return 0;
+}
+
 int main( int argc, char *argv[] )
 {
+	
+	char *from, *to, *name;
 
-	if( argc == 1)
-	{
-		printf("please use parameter!  use -h for help\n");
-		return 0;
+	getcwd(cwd, 1024);
+
+	printf("Please enter the password: ");
+	scanf("%s", password);
+	if (strcmp(password,PASSWORD) != 0) {
+		printf("Wrong password!\n");
+		return -1;
 	}
-	else if(argc==2) 
-	{
-		if (strncmp(argv[1],"ls",2)==0)
-		{
-		//list of files in safe
+	printf("\n\tWelcome to safe manager!\n");
 
-			getFileName(SAFEPATH);
+	while(1) {
+		fflush(stdin);
 
-			return 0;
-		}
-		else if(strncmp(argv[1],"-h",2)==0)
-		{
-			printf("./safe_manager in  filename     :cp file to /root/safeBox/\n");
-			printf("./safe_manager out filename     :cp file in /root/safeBox/ to current folder\n");
-			printf("./safe_manager ls               :check the file list in /root/safeBox/\n");
-		}
-		else 
-		{
-			printf("wrong list!  %s\n",argv[1]);
-			return 0;
-		}
-	}
-	else if (argc==3)
-	{
+		printf(">>> ");
+		scanf("%s", cmd1);
 
-		if(strncmp(argv[1],"in",2)!=0&&strncmp(argv[1],"out",3)!=0&&strncmp(argv[1],"del",3)!=0)
-		{
-			printf("wrong !  %s\n",argv[1]);
-			return 0;
+		if (strcmp(cmd1,"help") == 0 || strcmp(cmd1,"h") == 0) {
+			printf("in  filename    	:cp file into safe box\n");
+			printf("out filename    	:cp file from safe box to current folder\n");
+			printf("del filename    	:cp file from safe box to current folder\n");
+			printf("ls              	:check the file list in the safe box\n");
 		}
 
-		char *path1=argv[2];
-		char path2[80]=SAFEPATH;
-		char *p=strrchr(path1,'/');
-
-		if(p==NULL)
-		{
-			strcat(path2,"/");
-			strcat(path2,path1);
-		}
-		else
-		{
-			strcat(path2,p);
-		}
-		//printf("5wr %s ",aa);
-
-		int in=-1;
-		int out=-1;
-		int flag;
-		char buffer[1024];
-        if(strncmp(argv[1],"del",3) == 0)
-		{
-			out=remove(path2);
-printf("%s\n",path1);
-printf("%s\n",path2);
-			if(out==0)
+		else if (strcmp(cmd1,"in") == 0) {
+			scanf("%s", cmd2);
+			from = cmd2;
+			to = (char *)malloc(sizeof(char)*80);
+			strcpy(to, SAFEPATH);
+			name = strrchr(from,'/');
+			if(name == NULL)
 			{
+				strcat(to,"/");
+				strcat(to,from);
+			}
+			else
+				strcat(to,name);
+
+			//printf("copy from %s to %s\n", from, to);
+
+			if (copyFile(from,to) == -1)
+				printf("copy file failed!\n");
+			else
+				printf("copy file successfully!\n");
+
+			free(to);
+			to = NULL;
+		}
+
+		else if (strcmp(cmd1,"out") == 0) {
+			scanf("%s", cmd2);
+			to = cmd2;
+			from = (char *)malloc(sizeof(char)*80);
+			strcpy(from, SAFEPATH);
+			name = strrchr(to,'/');
+			if(name == NULL)
+			{
+				strcat(from,"/");
+				strcat(from,to);
+			}
+			else
+				strcat(from,name);
+
+			//printf("copy from %s to %s\n", from, to);
+
+			if (copyFile(from,to) == -1)
+				printf("copy file failed!\n");
+			else
+				printf("copy file successfully!\n");
+
+			free(from);
+			from = NULL;
+		}
+
+		else if (strcmp(cmd1,"del") == 0) {
+			scanf("%s", cmd2);
+			name = cmd2;
+			from = (char *)malloc(sizeof(char)*80);
+			strcpy(from,SAFEPATH);
+			strcat(from,"/");
+			strcat(from,name);
+
+			printf("delete file %s.\n", from);
+
+			if(remove(from) == 0)
 				printf("delete successfully!\n");
-				return 0;
-			}
-			return -1;
-		}
-		if(strncmp(argv[1],"out",3) == 0)
-		{
-			in = open(path2,S_IWUSR);
-			if(in == -1)
-			{ 
-				printf("open file %s failed!\n", path2); 
-				return -1;
-			}
-			out = creat(path1,S_IWUSR|S_IRUSR);
-			if (out == -1) 
-			{ 
-				printf("create file %s failed!\n", path1); 
-				return -1;
-			}
-		}
-		else if(strncmp(argv[1],"in",2) == 0)
-		{
-			in = open(path1,S_IWUSR);
-			if(in == -1)
-			{
-				printf("open file %s failed!\n", path1);
-				return -1;
-			} 
-			out = creat(path2,S_IWUSR|S_IRUSR);
-			if (out == -1)
-			{
-				printf("create file %s failed!\n", path2);
-				return -1;
-			}
+			else
+				printf("delete failed!\n");
 		}
 
-		// copy file content
-		while((flag=read(in,buffer,1024))>0)
-		{
-			write(out,buffer,flag);
+		else if (strcmp(cmd1,"ls") == 0) {
+			getFileName(SAFEPATH);
 		}
 
-		close(in);
-		close(out);
-		
-		printf("copy file successfully!\n");
+		else if (strcmp(cmd1,"exit") == 0) 
+			break;
+
+		else
+			goto wrong;
+
+
+		continue;
+
+
+wrong:
+
+		printf("Invalid syntax. Use 'h' or 'help' for help\n");
 	}
-	else
-	{
-		printf("wrong!\n");
-	}
+
 
 	return 0;
 }
