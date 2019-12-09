81b3238472fccddcc47cd70c319154752b2ed2b4
zhc <1553986076@qq.com>
2019-12-09 18:42:09 +0800

    log

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ make
 
 insmod hook.ko
 
-path：/root/safeBox
+path：/home/test/safeBox
 
-已成功hook read，write，openat，rename，unlinkat等系统调用，并成功实现保险箱管理程序的识别
+已成功hook openat，rename，unlinkat等系统调用，并成功实现保险箱管理程序的识别
 
 其他程序无法读取保险箱文件夹内文件内容，并且无法从文件外向保险箱传输文件
 
@@ -15,7 +15,7 @@ path：/root/safeBox
 
 无法使用ls，mv，cp，rm等命令
 
-
+产生日志文件路径：/home/test/safebox_log
 
 管理程序用法：
 
--- hook.c
+++ hook.c
@@ -28,7 +28,7 @@ MODULE_DESCRIPTION("hook syscalls");
 #define SAFENAME "safeBox"
 #define COMMANDNAME "safe_manager"
 #define MAX_LENGTH 256
-
+#define MY_FILE "/home/test/safebox_log"
 
 typedef asmlinkage long (*orig_openat_t)(struct pt_regs *regs);
 typedef asmlinkage long (*orig_rename_t)(struct pt_regs *regs);
@@ -40,7 +40,6 @@ orig_rename_t old_rename = NULL;
 orig_unlinkat_t old_unlinkat = NULL;
 unsigned int level;
 pte_t *pte;
-
 
 void get_path_from_dentry(char *path, struct dentry *dentry) {
 	char *buf = (char*)kmalloc(MAX_LENGTH,GFP_KERNEL);
@@ -66,6 +65,13 @@ void get_path_from_dentry(char *path, struct dentry *dentry) {
 asmlinkage long hooked_openat(struct pt_regs *regs) {
 	char *name = (char*)kmalloc(MAX_LENGTH,GFP_KERNEL);
 	char *path = (char*)kmalloc(MAX_LENGTH,GFP_KERNEL);
+	unsigned char buf[MAX_LENGTH]="\0";
+	mm_segment_t old_fs;
+	struct file* file=NULL;
+    struct kstat *stat;
+	loff_t pos; 
+	
+	
 	strncpy_from_user(name,(char*)regs->si,MAX_LENGTH);
 
 	task_lock(current);
@@ -91,6 +97,21 @@ asmlinkage long hooked_openat(struct pt_regs *regs) {
 	if (strncmp(path,SAFEPATH,strlen(SAFEPATH)-1) == 0 || strstr(path,SAFENAME)) {
 		if (strncmp(current->comm,COMMANDNAME,strlen(COMMANDNAME)-1) != 0) {
 			printk("Find openat operation to safeBox. The path is %s by %s \n",path,current->comm);
+			file = filp_open(MY_FILE,O_RDWR|O_CREAT,0777);
+            if (file==NULL)
+			{
+                printk("error occured while opening file %s, exiting...\n", MY_FILE);
+                return -1;
+            }
+			sprintf(buf,"Find openat operation to safeBox. The path is %s by %s\n",path,current->comm);
+			old_fs = get_fs();
+			set_fs(KERNEL_DS);
+			stat =(struct kstat *) kmalloc(sizeof(struct kstat),GFP_KERNEL);
+			vfs_stat(MY_FILE,stat);
+			pos = stat->size; 
+			vfs_write(file, buf, sizeof(buf), &pos);
+			set_fs(old_fs);
+			filp_close(file, NULL);
 			kfree(name);
 			kfree(path);
 			task_unlock(current);
@@ -109,6 +130,11 @@ asmlinkage long hooked_rename(struct pt_regs *regs) {
 	char *dst = (char*)kmalloc(MAX_LENGTH,GFP_KERNEL);
 	char *src = (char*)kmalloc(MAX_LENGTH,GFP_KERNEL);
 	char *path = (char*)kmalloc(MAX_LENGTH,GFP_KERNEL);
+	unsigned char buf[MAX_LENGTH]="\0";
+	mm_segment_t old_fs;
+	struct file* file=NULL;
+    struct kstat *stat;
+	loff_t pos; 
 	struct dentry *parent_dentry = current->fs->pwd.dentry;
 	strncpy_from_user(dst,(char*)regs->si,MAX_LENGTH);
 	strncpy_from_user(src,(char*)regs->di,MAX_LENGTH);
@@ -117,7 +143,22 @@ asmlinkage long hooked_rename(struct pt_regs *regs) {
 	get_path_from_dentry(path,parent_dentry);
 	if(strstr(path,SAFENAME))
 	{
-                printk("Find rename operation to safeBox. The path is %s, src:%s, dst:%s by %s\n",path,src,dst,current->comm);
+        printk("Find rename operation to safeBox. The path is %s, src:%s, dst:%s by %s\n",path,src,dst,current->comm);
+        file = filp_open(MY_FILE,O_RDWR|O_CREAT,0777);
+		if (file==NULL)
+		{
+			printk("error occured while opening file %s, exiting...\n", MY_FILE);
+			return -1;
+		}
+		sprintf(buf,"Find rename operation to safeBox. The path is %s, src:%s, dst:%s by %s\n",path,src,dst,current->comm);
+		old_fs = get_fs();
+		set_fs(KERNEL_DS);
+		stat =(struct kstat *) kmalloc(sizeof(struct kstat),GFP_KERNEL);
+		vfs_stat(MY_FILE,stat);
+		pos = stat->size; 
+		vfs_write(file, buf, sizeof(buf), &pos);
+		set_fs(old_fs);
+		filp_close(file, NULL);
 		kfree(path);
 		kfree(dst);
 		kfree(src);
@@ -128,11 +169,26 @@ asmlinkage long hooked_rename(struct pt_regs *regs) {
 	{
 		if (strncmp(current->comm,COMMANDNAME,strlen(COMMANDNAME)-1) != 0)
 		{
-                printk("Find rename operation to safeBox. The path is %s, src:%s, dst:%s by %s\n",path,src,dst,current->comm);
-		kfree(path);
-		kfree(dst);
-		kfree(src);
-		return -1;
+            printk("Find rename operation to safeBox. The path is %s, src:%s, dst:%s by %s\n",path,src,dst,current->comm);
+			file = filp_open(MY_FILE,O_RDWR|O_CREAT,0777);
+            if (file==NULL)
+			{
+                printk("error occured while opening file %s, exiting...\n", MY_FILE);
+                return -1;
+            }
+			sprintf(buf,"Find rename operation to safeBox. The path is %s, src:%s, dst:%s by %s\n",path,src,dst,current->comm);
+			old_fs = get_fs();
+			set_fs(KERNEL_DS);
+			stat =(struct kstat *) kmalloc(sizeof(struct kstat),GFP_KERNEL);
+			vfs_stat(MY_FILE,stat);
+			pos = stat->size; 
+			vfs_write(file, buf, sizeof(buf), &pos);
+			set_fs(old_fs);
+			filp_close(file, NULL);
+			kfree(path);
+			kfree(dst);
+			kfree(src);
+			return -1;
 		}
 	}	
 	kfree(path);
@@ -143,6 +199,12 @@ asmlinkage long hooked_rename(struct pt_regs *regs) {
 asmlinkage long hooked_unlinkat(struct pt_regs *regs) {
 	char *name = (char*)kmalloc(MAX_LENGTH,GFP_KERNEL);
 	char *path = (char*)kmalloc(MAX_LENGTH,GFP_KERNEL);
+	unsigned char buf[MAX_LENGTH]="\0";
+	mm_segment_t old_fs;
+	struct file* file=NULL;
+    struct kstat *stat;
+	loff_t pos; 
+
 	strncpy_from_user(name,(char*)regs->si,MAX_LENGTH);
 
 	task_lock(current);
@@ -168,6 +230,22 @@ asmlinkage long hooked_unlinkat(struct pt_regs *regs) {
 	if (strncmp(path,SAFEPATH,strlen(SAFEPATH)-1) == 0 || strstr(path,SAFENAME)) {
 		if (strncmp(current->comm,COMMANDNAME,strlen(COMMANDNAME)-1) != 0) {
 			printk("Find unlinkat operation to safeBox. The path is %s by %s\n",path,current->comm);
+            file = filp_open(MY_FILE,O_RDWR|O_CREAT,0777);
+            if (file==NULL)
+			{
+                printk("error occured while opening file %s, exiting...\n", MY_FILE);
+                return -1;
+            }
+			sprintf(buf,"Find unlinkat operation to safeBox. The path is %s by %s\n",path,current->comm);
+			old_fs = get_fs();
+			set_fs(KERNEL_DS);
+			stat =(struct kstat *) kmalloc(sizeof(struct kstat),GFP_KERNEL);
+			vfs_stat(MY_FILE,stat);
+			pos = stat->size; 
+			vfs_write(file, buf, sizeof(buf), &pos);
+			set_fs(old_fs);
+			filp_close(file, NULL);
+
 			kfree(name);
 			kfree(path);
 			task_unlock(current);
@@ -207,7 +285,6 @@ static int hooked_init(void) {
 		printk("- unable to locate sys_call_table\n");
 		return 0;
 	}
-
     printk("+ found sys_call_table at %08lx!\n", sys_call_table_addr);
  
     old_openat = ((orig_openat_t *)(sys_call_table_addr))[__NR_openat]; 
@@ -230,7 +307,8 @@ static int hooked_init(void) {
     printk("+ sys_unlinkat hooked!\n");
 	
     set_pte_atomic(pte, pte_clear_flags(*pte, _PAGE_RW));
- 
+
+	
     return 0;
 }
 
@@ -246,7 +324,8 @@ static void hooked_exit(void) {
     ((unsigned long * ) (sys_call_table_addr))[__NR_rename] = (unsigned long) old_rename;
 	((unsigned long * ) (sys_call_table_addr))[__NR_unlinkat] = (unsigned long) old_unlinkat;
     set_pte_atomic(pte, pte_clear_flags(*pte, _PAGE_RW));
-    
+
+               
     printk("+ Unloading hook.ko\n");
 }
 
